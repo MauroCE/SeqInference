8eeda38bc87f873b6d576f00689ed34afe480eae
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2020-06-04 16:23:45 +0100

    updated parameters

--- pmmh.cpp
+++ pmmh.cpp
@@ -34,7 +34,7 @@ public:
     rowvec curr_particles = this->_particles.row(t - 1);
     double mean, sd;
     for (int i = 0; i < this->_num_particles; ++i) {
-      mean = this->_param(0) * curr_particles(i);
+      mean = sqrt(this->_param(0)) * curr_particles(i);
       sd = sqrt(this->_param(2));
       propagate(i) = Rcpp::rnorm(1, mean, sd)[0];
     }
@@ -65,7 +65,7 @@ public:
     vec lik(this->_num_particles);
     double state_mean, lik_sd;
     for (int i = 0; i < this->_num_particles; ++i) {
-      state_mean = this->_param(0) * curr_particles(i);
+      state_mean = sqrt(this->_param(0)) * curr_particles(i);
       lik_sd = this->_param(1) * exp(state_mean / 2);
       lik(i) = Rcpp::dnorm(obsv, 0.0, lik_sd)[0];
     }
@@ -178,7 +178,7 @@ double logqeval(double thetastar, double thetagiven){
 
 // [[Rcpp::export(name="pmmh_cpp")]]
 Rcpp::List pmmh(double thetastart, int niter, int N, vec y) {  // N is the number of particles
-  vec param = {0.91, thetastart, 1.0};
+  vec param = {0.8281, thetastart, 1.0};
   
   // INITIALIZATION: Run APF and grab sample & log marginal, then set starting param
   Rcpp::List out = APF(y, N, param); // Run APF
@@ -197,7 +197,7 @@ Rcpp::List pmmh(double thetastart, int niter, int N, vec y) {  // N is the numbe
   for (int i=0; i < niter; i++){
     // Sample theta* from q(theta* | theta)
     theta_candidate = q(theta);
-    param = {0.91, theta_candidate, 1.0};
+    param = {0.8281, theta_candidate, 1.0};
     // Sample a candidate by running APF. Extract sample and log marginal
     out = APF(y, N, param);
     x_candidate = Rcpp::as<Col<double>>(out["sample"]);
