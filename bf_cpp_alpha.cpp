f4138902edece31e318b7b4556f5504f5c49d22c
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2020-06-06 15:04:46 +0100

    comments and rearrangement

--- bf_cpp_alpha.cpp
+++ bf_cpp_alpha.cpp
@@ -26,36 +26,28 @@ NumericVector transition(NumericVector x, double alpha, double sigma){
 List bf_cpp(NumericVector y, int N, double beta, double alpha, double sigma) {
   int tmax = y.size();
   // Initialize particles, resampled particles and weights
-  NumericMatrix particles(tmax+1, N);
-  NumericMatrix resampled(tmax+1, N);
-  NumericMatrix posterior_sample(tmax+1, N);  // Sample from the final posterior. Used by PMMH
-  NumericMatrix logweights(tmax+1, N);
-  NumericMatrix weightsnorm(tmax+1, N);
-  IntegerVector ix(N);
-  double log_marginal = 0.0;
-  double maxlogw, sumweights;
-  // First iteration
+  NumericMatrix particles(tmax+1, N);         // Stores the N particles for all tmax steps
+  NumericMatrix resampled(tmax+1, N);         // Temporary matrix storing the particles when resampling
+  NumericMatrix posterior_sample(tmax+1, N);  // Sample from the final posterior. Used by PMMH.
+  NumericMatrix logweights(tmax+1, N);        // N unnormalized logweights (one per particle) at each time step
+  NumericMatrix weightsnorm(tmax+1, N);       // N *normalized* logweights (one per particle) at each time step
+  IntegerVector ix(N);                        // Resampled indeces, one per particle.
+  double log_marginal = 0.0;                  // log p(y | theta) used by PMMH
+  double maxlogw, sumweights;                 // used for log-sum-exp trick and numerical stability when computing weights
+  // First iteration comes from the prior distribution
   particles.row(0) = prior(N, alpha, sigma);
-  //std::cout << "prior " << (NumericVector)particles.row(0) << std::endl;
-  //std::cout << "prior func " << prior(N, alpha, sigma) << std::endl;
   // Main loop
   for (int t=1; t < (tmax+1); t++){
     // Sample from the prior and calculate (normalized) weights 
-    //std::cout << "iteration " << t << std::endl;
     particles.row(t) = transition(particles.row(t-1), alpha, sigma);
-    //std::cout << "parts " << (NumericVector)particles.row(t) << std::endl;
     logweights.row(t) = likelihood(y[t], particles.row(t), beta);
-    //std::cout << "logweights" << (NumericVector)logweights.row(t) << " " << std::endl;
-    // log sum exp to find wait and likelihood
+    // Log-Sum-Exp trick to find wait and likelihood
     maxlogw = max(logweights.row(t));
     logweights.row(t) = exp(logweights.row(t) - maxlogw);
     sumweights = sum(logweights.row(t));
     weightsnorm.row(t) = logweights.row(t) / sumweights;
     log_marginal += maxlogw + log(sumweights) - log(N);
     // Sample indices based on weights and use them to resample the columns of particle
-    //std::ofstream outfile;
-    //outfile.open("weights.txt", std::ios_base::app); // append instead of overwrite
-    //outfile << "Range of NormWeights: " << (NumericVector)range(weightsnorm.row(t)); 
     ix = sample(N, N, true, (NumericVector)weightsnorm.row(t));
     for (int j=0; j < N; j++){
       resampled.column(j) = particles.column(ix[j]-1);
@@ -79,21 +71,19 @@ List bf_cpp(NumericVector y, int N, double beta, double alpha, double sigma) {
 // Sample theta proposal q(theta* | theta) In our case theta is beta. 
 // [[Rcpp::export(name="param_proposal")]]
 double qalpha(double thetagiven){
-  //return Rcpp::rgamma(1, 10, thetagiven/10)[0]; 
-  //R::rnorm(thetagiven, 0.5);
   return Rcpp::rbeta(1, 50, 50/thetagiven - 50)[0];
 }
 
 // Prior for theta. Evaluates prior density
 // [[Rcpp::export(name="logp")]]
 double logpalpha(double theta){
-  return R::dbeta(theta, 6, 6/0.8 - 6, true); //R::dnorm(theta, 0.9, 0.5, true);
+  return R::dbeta(theta, 6, 6/0.8 - 6, true);
 }
 
 // Evaluates q(theta*|theta)
 // [[Rcpp::export(name="logqeval")]]
 double logqalphaeval(double thetastar, double thetagiven){
-  return R::dbeta(thetastar, 50, 50/thetagiven - 50, true);   //R::dgamma(thetastar, 10, thetagiven/10, true); //R::dnorm(thetastar, thetagiven, 0.5, true);
+  return R::dbeta(thetastar, 50, 50/thetagiven - 50, true);
 }
 
  // SIGMA 
@@ -127,16 +117,16 @@ List pmmh(double thetastart, int niter, int N, NumericVector y, int burnin, doub
   
   // INITIALIZATION: Run APF and grab sample & log marginal, then set starting param
   List out = bf_cpp(y, N, beta, alphastart, thetastart); // Run APF
-  NumericVector x = out["filtered_states"];             // Grab a sample from posterior
-  double logm = out["log_marginal"];  // Grab log marginal
-  double theta = thetastart;           // Set initial parameter
+  NumericVector x = out["filtered_states"];              // Grab a sample from posterior
+  double logm = out["log_marginal"];                     // Grab log marginal
+  double theta = thetastart;                             // Set initial parameter
   double alpha = alphastart;
-  NumericVector logu = log(runif(N));             // Generate log() of uniform random numbers
-  NumericVector samples(niter);                // Instantiate a vector of samples. This will be outputted
+  NumericVector logu = log(runif(N));                    // Generate log() of uniform random numbers
+  NumericVector samples(niter);                          // Instantiate a vector of samples. This will be outputted
   NumericVector alphasamples(niter);
-  int accepted = 0;                 // Counts the number of times we accept
+  int accepted = 0;                                      // Counts the number of times we accept
   int alphaaccepted = 0;
-  NumericVector log_marginals(niter);         // Store the log marginals. Can be used to evaluate performance of algorithm
+  NumericVector log_marginals(niter);                      // Store the log marginals. Can be used to evaluate performance of algorithm
   double theta_candidate;
   double alpha_candidate;
   double logm_candidate;
@@ -148,16 +138,12 @@ List pmmh(double thetastart, int niter, int N, NumericVector y, int burnin, doub
   // BURN IN
   for (int i=0; i < burnin; i++){
     // Sample theta* from q(theta* | theta)
-    //std::cout << "write " << theta << " something" << std::endl;
     theta_candidate = qsigma(theta);
     alpha_candidate = qalpha(alpha);
-    //std::cout << "Cand: " << theta_candidate << std::endl;
-    //inbound = (0.0001 < theta_candidate) && (theta_candidate < 0.9999) && (theta_candidate != 1);
     inbound = (0.0001 < theta_candidate) && (theta_candidate != 0);
     inboundalpha = (0.0001 < alpha_candidate) && (alpha_candidate < 0.9999) && (alpha_candidate != 1);
     if (inbound && inboundalpha){
       // Sample a candidate by running APF. Extract sample and log marginal
-      //std::cout << "The eagle is in the nest, I repeat the eagle is in the nest." << std::endl;
       out = bf_cpp(y, N, beta, alpha_candidate, theta_candidate);
       x_candidate = out["sample"];
       logm_candidate = out["log_marginal"];
@@ -165,7 +151,6 @@ List pmmh(double thetastart, int niter, int N, NumericVector y, int burnin, doub
     ap = logm_candidate - logm
       + logpsigma(theta_candidate) - logpsigma(theta)  + logqsigmaeval(theta, theta_candidate) - logqsigmaeval(theta_candidate, theta) + log(inbound)
       + logpalpha(alpha_candidate) - logpalpha(alpha)  + logqalphaeval(alpha, alpha_candidate) - logqalphaeval(alpha_candidate, alpha) + log(inboundalpha);
-    //std::cout << "iteration: " << i << " Theta candidate: " << theta_candidate << " P(cand): " << exp(logpsigma(theta_candidate)) << " P(curr): "<< exp(logpsigma(theta)) << " AP: "<< ap << std::endl;
     // Compute acceptance ratio
     if (logu[i] <= ap){
       // Accept!
@@ -175,28 +160,21 @@ List pmmh(double thetastart, int niter, int N, NumericVector y, int burnin, doub
       logm = logm_candidate;
     }
   }
-  //std::cout << "burn in done" << std::endl;
-  
+
   // MAIN LOOP
   for (int i=0; i < niter; i++){
     // Sample theta* from q(theta* | theta)
     theta_candidate = qalpha(theta);
     alpha_candidate = qalpha(alpha);
-    //std::cout << "Cand: " << theta_candidate << std::endl;
-    // check 0 < < 1
-    //inbound = (0.0001 < theta_candidate) && (theta_candidate < 0.9999) && (theta_candidate != 1);
     inboundalpha = (0.0001 < alpha_candidate) && (alpha_candidate < 0.9999) && (alpha_candidate != 1);
     if (inbound && inboundalpha){
       // Sample a candidate by running APF. Extract sample and log marginal
-      //std::cout << "The eagle is in the nest, I repeat the eagle is in the nest." << std::endl;
       out = bf_cpp(y, N, beta, alpha_candidate, theta_candidate);
       x_candidate = out["sample"];
       logm_candidate = out["log_marginal"];
       
     }
     // Sample a candidate by running APF. Extract sample and log marginal
-    //std::cout << "iteration: " << i << " Theta candidate: " << theta_candidate << "P(cand): " << exp(logpsigma(theta_candidate)) << "P(curr): "<< exp(logpsigma(theta)) << " AP: "<< ap  << std::endl;
-    //ap = logm_candidate + logpalpha(theta_candidate) - logm - logpalpha(theta) + logqalphaeval(theta, theta_candidate) - logqalphaeval(theta_candidate, theta) + log(inbound);
     ap = logm_candidate - logm
       + logpsigma(theta_candidate) - logpsigma(theta)  + logqsigmaeval(theta, theta_candidate) - logqsigmaeval(theta_candidate, theta) + log(inbound)
       + logpalpha(alpha_candidate) - logpalpha(alpha)  + logqalphaeval(alpha, alpha_candidate) - logqalphaeval(alpha_candidate, alpha) + log(inboundalpha);
